5ff7f2866d58e6b179d5fe4babe0d17b38aa494a
niccneimi <niccneimi@gmail.com>
2025-01-20 01:23:57 +0300

    added profile/update and avatars

--- src/main/java/com/tutoras/tutoras/controller/AuthController.java
+++ src/main/java/com/tutoras/tutoras/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.tutoras.tutoras.controller;
 import org.springframework.web.bind.annotation.RestController;
 import com.tutoras.tutoras.model.LoginRequest;
 import com.tutoras.tutoras.model.LoginResponse;
+import com.tutoras.tutoras.model.ProfileRequest;
 import com.tutoras.tutoras.security.UserPrincipal;
 import com.tutoras.tutoras.service.AuthService;
+import com.tutoras.tutoras.service.ProfileService;
 import com.tutoras.tutoras.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,7 +17,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class AuthController {
 
     private final AuthService authService;
     private final UserService userService;
+    private final ProfileService profileService;
 
     @PostMapping("/api/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) {
@@ -32,6 +36,18 @@ public class AuthController {
     @GetMapping("/api/profile")
     public ResponseEntity<?> getProfile(@AuthenticationPrincipal UserPrincipal principal) {
         return userService.getProfile(principal.getEmail());
+    }
+
+    @PutMapping("/api/profile/update")
+    public ResponseEntity<?> updateProfile(
+        @AuthenticationPrincipal UserPrincipal principal, 
+        @ModelAttribute ProfileRequest request) {
+        return profileService.updateProfile(
+            principal.getUserId(), 
+            request.getFirstName(), 
+            request.getLastName(), 
+            request.getExtraInfo(), 
+            request.getAvatar());
     }
     
     
---
+++ src/main/java/com/tutoras/tutoras/service/ProfileService.java
@@ -0,0 +1,38 @@
+package com.tutoras.tutoras.service;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.tutoras.tutoras.entity.UserEntity;
+import com.tutoras.tutoras.model.ErrorResponse;
+import com.tutoras.tutoras.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ImageService imageService;
+    private final UserRepository userRepository;
+
+    public ResponseEntity<?> updateProfile(Long userId, String firstName, String lastName, String extraInfo, MultipartFile avatar) {
+        String avatarFileName = null;
+        if (avatar != null) {
+            try {
+                avatarFileName = imageService.saveImage("src/main/resources/static/images/avatars", avatar);
+            } catch (IOException e) {
+                ErrorResponse errorResponse = new ErrorResponse(401L, "Ошибка загрузки изображения");
+                return ResponseEntity.status(401).body(errorResponse);
+            }
+        }
+        UserEntity user = userRepository.findById(userId).get();
+        UserEntity updatedEntity = new UserEntity(userId, firstName, lastName, extraInfo, avatarFileName, user.getEmail(), user.getPassword(), user.getRole(), user.getEvents(), user.getEventsAsFollower());
+        userRepository.save(updatedEntity);
+        return ResponseEntity.ok().build();
+
+    }
+}
